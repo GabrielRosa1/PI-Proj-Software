12ab9f0ac65306598cebb3635e4484b9ee877708
GabrielRosa1 <gabrielrosamart@gmail.com>
2026-09-23 00:34:19 -0300

    Esqueleto pronto para deploy

---
+++ src/main/java/com/pi/zambom/PiProjSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.pi.zambom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiProjSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiProjSoftwareApplication.class, args);
+	}
+
+}
